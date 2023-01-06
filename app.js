6b9dfc0cdd3d18df585e0370e62a84f67d325ef5
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-01-06 18:06:14 -0500

    Advanced routes and forms

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath)) 
+
+
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/home.html'));
+});
+
+app.get('/register',(req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'));
+});
+
+app.get('/login',(req,res) =>{
+    res.sendFile(path.join(__dirname,'./views/login.html'));
+});
+
+
+app.listen(8000, () => console.log("Servidor funcionando en puerto 8000"));
+
+
+
+
+
+
+
+
