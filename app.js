7b6696561da2722ba34adf3043f91a8b6155ea68
arestrepogiraldo2200 <arestrepogiraldo@gmail.com>
2023-01-08 18:54:28 -0500

    Entrega 1 frontend

--- app.js
+++ app.js
@@ -19,6 +19,17 @@ app.get('/login',(req,res) =>{
     res.sendFile(path.join(__dirname,'./views/login.html'));
 });
 
+app.post('/register', (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+});
+
+app.post('/login', (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+});
+
+app.post('/', (req,res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+});
 
 app.listen(8000, () => console.log("Servidor funcionando en puerto 8000"));
 
